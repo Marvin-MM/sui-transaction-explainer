9aaa06a202bacc435b929d8989e157e1f1e7c922
Marvin-MM <masabamarvin1@gmail.com>
2025-12-02 19:28:22 +0300

    add a sui icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,19 +27,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/sui-logo.svg",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/sui-logo.svg",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/sui-logo.svg",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/sui-logo.svg",
   },
 }
 
